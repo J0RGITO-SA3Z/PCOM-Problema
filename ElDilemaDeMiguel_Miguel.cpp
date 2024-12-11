bdcbcacb404087d4aa2ae6ef15765617c794fe81
Migui300 <114219353+Migui300@users.noreply.github.com>
2024-12-11 17:02:54 +0100

    ahora si, mi implementación pero comparando bien
    me ha dado casi perfecto, fallando en 2 líneas q creo q han sido cosa de clion, prueba tú en visual porfa jorge

--- ElDilemaDeMiguel_Miguel.cpp
+++ ElDilemaDeMiguel_Miguel.cpp
@@ -1,34 +1,53 @@
-// Nombre del alumno: Miguel Xuclá y Jorge Sáez
-// Usuario del Juez: PC01
-
+// Nombre del alumno Jorge Sáez Salto y Miguel Xuclá Herrero
+// Usuario del Juez PC01
+// El dilema de Miguel
 
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <vector>
-#include <queue>
-#include <cstring>
 #include <algorithm>
+#include <queue>
+#include <unordered_map>
+#include <cmath>
 
-const int INF = 1e9; // o cualquier valor grande suficiente
 
-std::vector<std::vector<int>> adj;
-std::vector<std::vector<int>> cap;
+const int INF = 1e9;
+
+int V;
+
+using vi = std::vector<int>;
+using vvi = std::vector<vi>;
+vvi adj;
+
 std::vector<int> parent;
+std::vector<std::vector<int>> cap;
 std::vector<bool> visited;
-//Cola para guardar las aristas ordenadas en función de su capacidad y en igualdad de condiciones, por orden de entrada
-std::priority_queue<std::pair<std::pair<int, int>, std::pair<int, int>>> aristasOrdenadas; // <<capacidad, horaLLegada>, <u, v>>
+
+struct tArista {
+
+    int capacidad;
+    int id;
+    int u;
+    int v; 
+
+    bool operator<(const tArista& other)const { 
+        if (capacidad == other.capacidad) {
+            return id > other.id; 
+        }
+        return  capacidad < other.capacidad;
+    }
+
+};
 
 
 void bfs(int s, int t) {
     std::queue<int> q;
-    visited.assign(visited.size(), false);
+    visited = std::vector<bool>(2 * V + 2, false);
     q.push(s);
-    parent[s] = -1;
-    visited[s] = true;
+    parent[s] = -1; visited[s] = true;
     while (!q.empty()) {
-        int u = q.front();
-        q.pop();
+        int u = q.front(); q.pop();
         if (u == t) break;
         for (int i = 0; i < adj[u].size(); ++i) {
             int v = adj[u][i];
@@ -36,26 +55,22 @@ void bfs(int s, int t) {
                 parent[v] = u;
                 visited[v] = true;
                 q.push(v);
-
             }
-
         }
-
     }
-
 }
 
 
 int sendFlow(int s, int t) {
     // Intentamos llegar de s a t
     bfs(s, t);
-    if (!visited[t]) return 0; // No pudimos
-    // Buscamos capacidad más pequeña en el camino
+    if (!visited[t])
+        return 0; // No pudimos
+    // Buscamos capacidad m´as peque˜na en el camino
     int flow = INF, v = t;
     while (v != s) {
-        flow = std::min(cap[parent[v]][v], flow);
+        flow = std::min(cap[parent[v]][v], flow); 
         v = parent[v];
-
     }
     // Mandamos flujo
     v = t;
@@ -63,10 +78,8 @@ int sendFlow(int s, int t) {
         cap[parent[v]][v] -= flow;
         cap[v][parent[v]] += flow; // INVERSA
         v = parent[v];
-
     }
     return flow;
-
 }
 
 int edmondsKarp(int s, int t) {
@@ -75,49 +88,54 @@ int edmondsKarp(int s, int t) {
     do {
         flow = sendFlow(s, t);
         ret += flow;
-
     } while (flow > 0);
     return ret;
 }
 
-void addArista(std::pair<std::pair<int, int>, std::pair<int, int>> arista){
-    adj[arista.second.first].push_back(arista.second.second);
-    cap[arista.second.first][arista.second.second] = arista.first.first;
-}
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
-    int N, M, Z, K;
+    int N ,M, Z, K;  
     std::cin >> N >> M >> Z;
     if (!std::cin)
         return false;
 
-    adj.assign(N + 1, std::vector<int>());
-    cap.assign(N + 1, std::vector<int>(N + 1, 0));
-    parent.assign(N + 1, -1);
-    visited.assign(N + 1, false);
-    aristasOrdenadas = std::priority_queue<std::pair<std::pair<int, int>, std::pair<int, int>>>();
+    V = N;
+    cap = std::vector<std::vector<int>>(V, std::vector<int>(V));
+    parent = std::vector<int>(V);
+    adj = std::vector<std::vector<int>>(V);
+    std::priority_queue<tArista> pq; 
 
-    int u, v, c;
-    for (int i = 0; i < M; ++i) {
-        std::cin >> u >> v >> c;
-        aristasOrdenadas.push({{c, i},{u, v}});
+    tArista aux; 
+    for (int i = 0; i < M; i++) {
+        std::cin >> aux.u >> aux.v >> aux.capacidad;
+        aux.u--; aux.v--;
+        aux.id = i;
+        adj[aux.u].push_back(aux.v);
+        adj[aux.v].push_back(aux.u);
+        cap[aux.u][aux.v] = aux.capacidad; 
+        pq.push(aux);
     }
 
     std::cin >> K;
-    for(int i = 0; i < K; ++i){
-        aristasOrdenadas.pop();
+
+    while (K > 0) {  
+        aux = pq.top();
+        cap[aux.u][aux.v] = 0; 
+        pq.pop();
+        K--;
     }
 
-    while(!aristasOrdenadas.empty()){
-        addArista(aristasOrdenadas.top());
-        aristasOrdenadas.pop();
-    }
+    int sol = edmondsKarp(0, N - 1); 
 
-    if(edmondsKarp(1, N) >= Z) std::cout << "Ya lo habia pensado\n";
-    else std::cout << "Es cierto, no se me habia ocurrido\n";
+    if (sol >= Z) {
+        std::cout << "Ya lo habia pensado" << '\n'; 
+    }
+    else {
+        std::cout << "Es cierto, no se me habia ocurrido" << '\n';
+    }
 
     return true;
 
@@ -129,10 +147,11 @@ int main() {
 #ifndef DOMJUDGE
     std::ifstream in("caso_prueba.txt");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif
+#endif 
 
 
-    while (resuelveCaso());
+    while (resuelveCaso())
+        ;
 
 
     // Para restablecer entrada. Comentar para acepta el reto
